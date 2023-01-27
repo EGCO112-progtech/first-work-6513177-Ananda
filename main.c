5156026e5188cdb950e5b3e8f82b56d4ee282579
6513177-Ananda <ananda.kon@student.mahidol.edu>
2023-01-27 14:30:30 +0700

    Just change something

--- main.c
+++ main.c
@@ -7,12 +7,12 @@
 
 #include <stdio.h>
 
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
     
-    printf("I have %d arguments\n",argc);
+    printf("argc is %d\n",argc);
     int i,N;
     for(i=1;i<argc;i++){
-        printf("arg %d: %s\n",i,argv[i]);
+        printf("%s\n",argv[i]);
         
     }
     
